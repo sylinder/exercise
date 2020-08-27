c4aed005f63e1d2aa180f9ac346fcc1a7d5d97e6
caps <yzsylinder@163.com>
2020-08-27 15:33:06 +0800

    [caps] refactor: extract formatFunction.

--- src/statement.js
+++ src/statement.js
@@ -29,15 +29,20 @@ function calculateAmount(play, perf) {
   return thisAmount;
 }
 
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+function formatFunction() {
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+  return format;
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = formatFunction();
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calculateAmount(play, perf);
