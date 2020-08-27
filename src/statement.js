fa6527d30b56a03e081bb55010ea1e02777303e9
caps <yzsylinder@163.com>
2020-08-27 15:18:23 +0800

    [caps] refactor: extract calculateAmount function.

--- src/statement.js
+++ src/statement.js
@@ -7,6 +7,27 @@ function calculateCredit(volumeCredits, perf, play) {
   return volumeCredits;
 }
 
+function calculateAmount(play, thisAmount, perf) {
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -19,23 +40,7 @@ function statement (invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    thisAmount = calculateAmount(play, thisAmount, perf);
     totalAmount += thisAmount;
     volumeCredits = calculateCredit(volumeCredits, perf, play);
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
